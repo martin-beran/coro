f74672bb34d5c98597ff61e121ff95ccc93318ad
Martin Beran <martin@mber.cz>
2022-10-31 21:03:02 +0100

    Initial commit
    - repository structure
    - .gitignore
    - build framework (CMake and Doxygen configuration)
    - documentation files
    - dummy source and test

---
+++ src/coro/coro.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+/*! \file
+ * \brief The main header file of the Coro library
+ */
+
+//! The top-level namespace of Coro
+namespace coro {
+
+//! This namespace contains various implementation details
+namespace impl {
+} // namespace impl
+
+} // namespace coro
