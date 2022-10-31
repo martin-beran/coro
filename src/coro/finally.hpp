2690758b5aee0a6dc5e8965741de122e5c410221
Martin Beran <martin@mber.cz>
2022-10-31 21:46:52 +0100

    A RAII wrapper for calling a function at a scope end

---
+++ src/coro/finally.hpp
@@ -0,0 +1,45 @@
+#pragma once
+
+/*! \file
+ * \brief A RAII function wrapper \ref coro::finally.
+ */
+
+#include <utility>
+
+namespace coro {
+
+namespace impl {
+
+//! Requirements for a template argument of coro::finally
+/*! \tparam F a callable type */
+template <class F>
+concept finally_fun = std::is_nothrow_invocable_v<F>;
+
+} // namespace impl
+
+//! A RAII function wrapper for a function that should be called at a scope end
+/*! It calls a function when the scope contaning an automatic variable of class
+ * \ref finally is ended, either normally or by an exception.
+ * \tparam F the type of the called function */
+template <impl::finally_fun F> class finally {
+public:
+    //! Registers a function to be called at a scope end.
+    /*! \param[in] f a function */
+    explicit finally(F&& f) noexcept: f(std::move(f)) {}
+    //! No copying
+    finally(const finally&) = delete;
+    //! No moving
+    finally(finally&&) = delete;
+    //! No copying
+    finally& operator=(const finally&) = delete;
+    //! No moving
+    finally& operator=(finally&&) = delete;
+    //! Calls the registered function
+    ~finally() {
+        f();
+    }
+private:
+    F f; //!< The registered function
+};
+
+} // namespace coro
